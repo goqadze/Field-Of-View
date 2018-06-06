80597bea8258d3383dad625d46e9f720ef58d383
goqadze <agoqadze@gmail.com>
2018-06-06 18:18:18 +0400

    field of view

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public float moveSpeed = 6;
+    Rigidbody rigidBody;
+    Camera viewCamera;
+    Vector3 velocity;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
+        Vector3 lookAt = mousePos + Vector3.up * transform.position.y;
+        if (Vector3.Distance(lookAt, transform.position) > 1f)
+            transform.LookAt(lookAt);
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+}
